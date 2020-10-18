a615a13e2d3f0feccca3d4a7afee397bc4520c7b
Dario Nascimento <dario.nascimento@cba.com.au>
2020-10-18 15:49:16 +1100

    argo

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -46,12 +46,13 @@ export class ClusterStack extends Stack {
     const argocdManifest = yaml.safeLoadAll(
       fs.readFileSync("./k8s-manifests/argocd.yaml", "utf8")
     );
-    cluster.addManifest("argocd", ...argocdManifest);
+    cluster.addManifest("argo", ...argocdManifest);
+
     const appsManifest = yaml.safeLoadAll(
       fs.readFileSync("./k8s-manifests/apps.yaml", "utf8")
     );
     appsManifest[1].spec.source.helm.parameters[0].value = props.stage;
-    cluster.addManifest("apps", ...appsManifest);
+    cluster.addManifest("argo-apps", ...appsManifest);
 
     this.clusterEndpoint = new CfnOutput(this, "Url", {
       value: cluster.clusterEndpoint,
