4b5a05b68aa67f7abf4c2d575f60819b4d1bc891
Dario Nascimento <dario.nascimento@cba.com.au>
2020-10-18 16:07:32 +1100

    remove

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -43,17 +43,17 @@ export class ClusterStack extends Stack {
       ],
     });
 
-    const argocdManifest = yaml.safeLoadAll(
-      fs.readFileSync("./k8s-manifests/argocd.yaml", "utf8")
-    );
-    const argocd = cluster.addManifest("argo", ...argocdManifest);
+    // const argocdManifest = yaml.safeLoadAll(
+    //   fs.readFileSync("./k8s-manifests/argocd.yaml", "utf8")
+    // );
+    // const argocd = cluster.addManifest("argo", ...argocdManifest);
 
-    const appsManifest = yaml.safeLoadAll(
-      fs.readFileSync("./k8s-manifests/apps.yaml", "utf8")
-    );
-    appsManifest[1].spec.source.helm.parameters[0].value = props.stage;
-    const apps = cluster.addManifest("argo-apps", ...appsManifest);
-    apps.node.addDependency(argocd);
+    // const appsManifest = yaml.safeLoadAll(
+    //   fs.readFileSync("./k8s-manifests/apps.yaml", "utf8")
+    // );
+    // appsManifest[1].spec.source.helm.parameters[0].value = props.stage;
+    // const apps = cluster.addManifest("argo-apps", ...appsManifest);
+    // apps.node.addDependency(argocd);
     this.clusterEndpoint = new CfnOutput(this, "Url", {
       value: cluster.clusterEndpoint,
     });
